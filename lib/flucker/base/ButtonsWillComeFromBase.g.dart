37609f3822c64ec5f7dd7c0a5c99b39985322bc7
Renz Christen Yeomer Pagulayan <larongbingo@gmail.com>
2023-02-26 03:21:33 +0800

    Chore: add generated files

---
+++ lib/flucker/base/ButtonsWillComeFromBase.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ButtonsWillComeFromBase.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ButtonsWillComeFromBase _$ButtonsWillComeFromBaseFromJson(
+        Map<String, dynamic> json) =>
+    ButtonsWillComeFromBase(
+      ButtonsDependsOnBase.fromJson(json['DependsOn'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$ButtonsWillComeFromBaseToJson(
+        ButtonsWillComeFromBase instance) =>
+    <String, dynamic>{
+      'DependsOn': instance.DependsOn,
+    };
